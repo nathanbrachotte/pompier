2ef54d15f0860b2bbc687079d6c13e8f3d53bb95
Nathan Brachotte <nathan.brachotte@gmail.com>
2018-08-18 16:33:35 +0200

    -

---
+++ flow-typed/index.js
@@ -0,0 +1,15 @@
+declare module 'history/createMemoryHistory' {
+  declare module.exports: any;
+}
+
+declare module 'redux-persist/integration/react' {
+  declare module.exports: any;
+}
+
+declare module 'redux-persist/lib/storage' {
+  declare module.exports: any;
+}
+
+declare module 'RNSK' {
+  declare module.exports: any;
+}
